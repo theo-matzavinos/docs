0eef127e64aff8c662c161a70a07f26838c56d25
Theophilos Matzavinos <tmatzavinos@singularlogic.eu>
2023-07-11 11:13:07 +0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://theo-matzavinos.github.io",
+  base: "/docs",
   integrations: [
     starlight({
       title: "Docs",
